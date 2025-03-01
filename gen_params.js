73bebda3d8f6cb9a5dc646e3c2b28b3a40fd1671
nornjuidat <njuidatnor750@gmail.com>
2025-03-02 00:49:44 +0200

    תיקון

--- gen_params.js
+++ gen_params.js
@@ -1,13 +1,4 @@
-const db = require('./db');
-const createUsers = () => {
-    const users = [
-        { name: 'אדם כהן' },
-        { name: 'דנה לוי' },
-        { name: 'יוסי מזרחי' }
-    ];
-    users.forEach(user => {
-        db.query('INSERT INTO users (name) VALUES (?)', [user.name], (err) => {
-            if (err) console.error('Error inserting user:', err);
-        });
-    });
-};
+exports.HOST        = "localhost";
+exports.USER        = "root";
+exports.PASSWORD    = "";
+exports.DATABASE    = "database";
