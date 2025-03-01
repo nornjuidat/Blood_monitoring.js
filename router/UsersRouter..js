f6bcb74369b9bfa372f9b7713a351e821461e22f
nornjuidat <njuidatnor750@gmail.com>
2025-03-02 01:01:48 +0200

    הוספת משתמש חדש

--- router/UsersRouter..js
+++ router/UsersRouter..js
@@ -7,3 +7,13 @@ usersRoutes.get('/', (req, res) => {
         res.json(results);
     });
 });
+usersRoutes.post('/', (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ error: 'Name is required' });
+    db.query('INSERT INTO users (name) VALUES (?)', [name], (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json({ id: results.insertId, name });
+    });
+});
+
+module.exports = usersRoutes;
